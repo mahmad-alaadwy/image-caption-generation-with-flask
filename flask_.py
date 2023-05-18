2af8ba624e6287c96bf85c63816e26fe5632f7ed
mahmad-alaadwy <104855133+mahmad-alaadwy@users.noreply.github.com>
2023-05-18 09:55:02 +0300

    cleaning un used comments ,code

--- flask_.py
+++ flask_.py
@@ -57,8 +57,7 @@ def run():
     model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
 
 
-    features = {}
-    with open('features.pkl', 'rb') as f:features = pickle.load(f)
+
 
     with open('captions.txt', 'r') as f:
         next(f)
@@ -120,36 +119,8 @@ def run():
     return model,tokenizer,max_length
 
 
-# mapping = create_mapping()
 
-# clean(mapping)
-
-# # this function seeks to generat a matrix of image ids ,caption as numerical 
-# # ,one hot  encoding of the words
-# # and will spilit them as ebochs
-# def data_generator(data_keys, mapping, features, tokenizer, max_length, vocab_size, batch_size):
-  
-# 	X1, X2, y = list(), list(), list()
-# 	n = 0
-# 	while 1:
-# 		for key in data_keys:
-# 			n += 1
-# 			captions = mapping[key]
-# 			for caption in captions:
-# 				seq = tokenizer.texts_to_sequences([caption])[0]
-# 				for i in range(1, len(seq)):
-# 					in_seq, out_seq = seq[:i], seq[i]
-# 					in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
-# 					out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
-# 					X1.append(features[key][0])
-# 					X2.append(in_seq)
-# 					y.append(out_seq)
-# 			if n == batch_size:
-# 				X1, X2, y = np.array(X1), np.array(X2), np.array(y)
-# 				yield [X1, X2], y
-# 				X1, X2, y = list(), list(), list()
-# 				n = 0
-		
+	
 def idx_to_word(integer, tokenizer):
     for word, index in tokenizer.word_index.items():
         if index == integer:
